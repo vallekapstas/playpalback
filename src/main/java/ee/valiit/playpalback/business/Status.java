da84719dc7f26f8c322e81ba8da2d077f32ca988
fredb <fred.brosman06@gmail.com>
2024-04-10 15:13:11 +0300

    Status dictionary created

---
+++ src/main/java/ee/valiit/playpalback/business/Status.java
@@ -0,0 +1,12 @@
+package ee.valiit.playpalback.business;
+
+public class Status {
+    public static final String ACTIVE = "A";    // General
+    public static final String APPROVED = "A";  // Participants, Tags
+    public static final String PENDING = "P";   // Participants, Tags, Event(BL)
+    public static final String REJECTED = "R";  // Participants, Tags
+    public static final String SUSPENDED = "S"; // User, Event(BL)
+    public static final String CANCELLED = "C"; // Event
+    public static final String DELETED = "D";   // General
+
+}
