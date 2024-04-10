97ca0cf6c16277d99bad60303feea996c49377b7
Valle Kapstas <valle.kapstas@gmail.com>
2024-04-10 10:11:38 +0300

    Initial Java Spring Boot project & Master branch creation.

---
+++ src/main/java/ee/valiit/playpalback/PlaypalbackApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.playpalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaypalbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaypalbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/playpalback/PlaypalbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.playpalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaypalbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
