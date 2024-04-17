26280924df59b84d3a60c2e342ea9641e834dce1
fredb <fred.brosman06@gmail.com>
2024-04-17 13:46:05 +0300

    PLYP-37 COMPLETE + added documentation

--- src/main/java/ee/valiit/playpalback/business/gender/GenderController.java
+++ src/main/java/ee/valiit/playpalback/business/gender/GenderController.java
@@ -1,6 +1,7 @@
 package ee.valiit.playpalback.business.gender;
 
 import ee.valiit.playpalback.business.gender.dto.GenderInfo;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,8 @@ public class GenderController {
     private final GenderService genderService;
 
     @GetMapping("/genders")
-
+    @Operation(summary = "Returns genderId and genderName",
+            description = "Returns a list of all the active genders. Filter by status = 'A' and sort by genderName ASC. ")
     public List<GenderInfo> getGenders() {
         return genderService.getGenders();
     }
