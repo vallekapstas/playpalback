b63f09c2414d3a188fc1bc058a84e595227da8d0
fredb <fred.brosman06@gmail.com>
2024-04-22 16:23:28 +0300

    plyp-42- removed @params from EventRepository

--- src/main/java/ee/valiit/playpalback/domain/event/event/EventRepository.java
+++ src/main/java/ee/valiit/playpalback/domain/event/event/EventRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    @Query("select e from Event e where e.id = :id and e.status <> :status")
-    Event getEventBy(@Param("id") Integer id, @Param("status") String status);
+    @Query("select e from Event e where e.id = :eventId and e.status <> :eventStatus")
+    Event getEventBy( Integer eventId,  String eventStatus);
 
 }
