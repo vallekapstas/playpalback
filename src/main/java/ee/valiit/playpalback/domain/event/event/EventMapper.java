4c9ea8994b508568ed866ba82b79ffe7465a082d
fredb <fred.brosman06@gmail.com>
2024-04-26 09:52:13 +0300

    PLYP-96-added event description to Get event service

--- src/main/java/ee/valiit/playpalback/domain/event/event/EventMapper.java
+++ src/main/java/ee/valiit/playpalback/domain/event/event/EventMapper.java
@@ -1,7 +1,6 @@
 package ee.valiit.playpalback.domain.event.event;
 
 import ee.valiit.playpalback.business.event.dto.EventInfoRequest;
-import ee.valiit.playpalback.business.participant.dto.AddParticipantToEventRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -30,6 +29,7 @@ public interface EventMapper {
     @Mapping(source = "location.city.county.name", target = "countyName")
     @Mapping(source = "location.city.county.country.name", target="countryName")
     @Mapping(source = "status", target = "eventStatus")
+    @Mapping(source="eventDescription", target = "eventDescription")
     EventInfoRequest toEventInfo(Event event);
 
 }
